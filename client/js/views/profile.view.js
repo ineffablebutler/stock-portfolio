42367ea1ca625a8dd4d2da7e00079f811acd3533
Francois Romain <francoisromain@gmail.com>
2015-07-02 22:45:20 -0700

    Add graph to profile view

--- client/js/views/profile.view.js
+++ client/js/views/profile.view.js
@@ -1,6 +1,8 @@
 var $ = require('jquery');
 var Backbone = require('backbone');
 var _ = require('underscore');
+var GraphView = require('./graph.view');
+var GraphTrendsModel = require('../models/graph.trends.model');
 Backbone.$ = $;
 var ProfileView = Backbone.View.extend({
   el: "#main",
@@ -12,6 +14,9 @@ var ProfileView = Backbone.View.extend({
     this.$el.html(_.template(this.template({
       title: this.model.get('title')
     })));
+    var graphView = new GraphView({model: new GraphTrendsModel() });
+    this.$("#graph").html(graphView.el);
+    graphView.render();
   }
 });
 module.exports = ProfileView;
