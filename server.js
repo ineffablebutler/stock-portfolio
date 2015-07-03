bc17a2f724a81830eb4ffd388d96f8d9db3e920a
Eric Kao <ericskao@gmail.com>
2015-07-03 11:49:42 -0700

    (refactor)- changed 'client' file name to 'public'

--- server.js
+++ server.js
@@ -32,7 +32,7 @@ app.use(bodyParser.urlencoded({
   extended: true
 }));
 app.use(bodyParser.json());
-app.use(express.static(__dirname + '/client'));
+app.use(express.static(__dirname + '/public'));
 app.use(passport.initialize());
 app.use(passport.session());
 app.use('/signout', signoutRouter);
