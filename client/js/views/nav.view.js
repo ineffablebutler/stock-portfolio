bd7c9ab03ecb66431979047aa8cee524a2a91a7c
Eric Kao <ericskao@gmail.com>
2015-07-03 16:33:16 -0700

    feat-Stock Model created and autocomplete feature

--- client/js/views/nav.view.js
+++ client/js/views/nav.view.js
@@ -1,10 +1,15 @@
-var $ = require('jquery');
+var $ = jQuery = require('jquery');
 var Backbone = require('backbone');
 var _ = require('underscore');
 Backbone.$ = $;
+var semantic = require('../../lib/semantic.js');
 var navView = Backbone.View.extend({
   el: "#nav",
+  events: {
+    "keyup .search" : "handleAutoComplete"
+  },
   initialize: function () {
+    // var search = this.model;
     this.template = require('../../templates/nav.template.html');
     this.render();
   },
@@ -15,6 +20,35 @@ var navView = Backbone.View.extend({
     })));
     $('#nav .item').removeClass('active');
     $('#nav #' + cat).addClass('active');
+  },
+  handleAutoComplete: function(){
+    var query = $(".prompt").val();
+    if(query.length>0){
+      console.log(query); 
+      $.ajax({
+          type: 'GET',
+          url:'/auto',
+          data: {data: query},
+          success: function (response) {
+            parsed = (JSON.parse(response.slice(39,response.length-1 ))).ResultSet.Result;
+            var suggestions = [];
+            //format array
+            for(var i = 0; i<parsed.length; i++){
+              suggestions.push({title:parsed[i].name + " (" + parsed[i].symbol + ")", symbol: parsed[i].symbol});
+            };
+            $('.ui.search').search({
+                source : suggestions,
+                searchFields: ['title', 'symbol'],
+                searchFullText: true,
+                cache: true,
+              });
+            // $('.ui.search').search({searchFields:['name','symbol'], source: suggestions });
+          },
+          error: function() {
+            console.log("nopee!");
+          }
+        });
+    }
   }
 });
 module.exports = navView;
